abde0dc50db356a280d9a3f4749d5ee0621d8fb7
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2023-06-14 15:42:12 +0200

    Update manifest

--- src/pages/api/manifest.ts
+++ src/pages/api/manifest.ts
@@ -6,16 +6,12 @@ import packageJson from "../../../package.json";
 export default createManifestHandler({
   async manifestFactory(context) {
     const manifest: AppManifest = {
-      name: packageJson.name,
+      id: "app.saleor.stripe",
+      name: "Stripe",
+      about: packageJson.description,
       tokenTargetUrl: `${context.appBaseUrl}/api/register`,
-      appUrl: `${context.appBaseUrl}/config`,
-      permissions: [
-        /**
-         * Set permissions for app if needed
-         * https://docs.saleor.io/docs/3.x/developer/permissions
-         */
-      ],
-      id: "saleor.app",
+      appUrl: `${context.appBaseUrl}`,
+      permissions: ["HANDLE_PAYMENTS"],
       version: packageJson.version,
       requiredSaleorVersion: ">=3.13",
       webhooks: [
@@ -27,13 +23,9 @@ export default createManifestHandler({
          * Easiest way to create webhook is to use app-sdk
          * https://github.com/saleor/saleor-app-sdk/blob/main/docs/saleor-async-webhook.md
          */
+        // TODO: Add webhooks
       ],
-      extensions: [
-        /**
-         * Optionally, extend Dashboard with custom UIs
-         * https://docs.saleor.io/docs/3.x/developer/extending/apps/extending-dashboard-with-apps
-         */
-      ],
+      extensions: [],
     };
 
     return manifest;
