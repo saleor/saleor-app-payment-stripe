b4b078cb31b1cd411fd63971896a3a4783f5dda1
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2023-06-15 10:44:40 +0200

    Update manifest to include webhooks

--- src/pages/api/manifest.ts
+++ src/pages/api/manifest.ts
@@ -2,6 +2,8 @@ import { createManifestHandler } from "@saleor/app-sdk/handlers/next";
 import { type AppManifest } from "@saleor/app-sdk/types";
 
 import packageJson from "../../../package.json";
+import { paymentGatewayInitializeSessionSyncWebhook } from "./webhooks/saleor/payment-gateway-initialize-session";
+import { transactionInitializeSessionSyncWebhook } from "./webhooks/saleor/transaction-initialize-session";
 
 export default createManifestHandler({
   async manifestFactory(context) {
@@ -15,15 +17,8 @@ export default createManifestHandler({
       version: packageJson.version,
       requiredSaleorVersion: ">=3.13",
       webhooks: [
-        /**
-         * Configure webhooks here. They will be created in Saleor during installation
-         * Read more
-         * https://docs.saleor.io/docs/3.x/developer/api-reference/objects/webhook
-         *
-         * Easiest way to create webhook is to use app-sdk
-         * https://github.com/saleor/saleor-app-sdk/blob/main/docs/saleor-async-webhook.md
-         */
-        // TODO: Add webhooks
+        paymentGatewayInitializeSessionSyncWebhook.getWebhookManifest(context.appBaseUrl),
+        transactionInitializeSessionSyncWebhook.getWebhookManifest(context.appBaseUrl),
       ],
       extensions: [],
     };
