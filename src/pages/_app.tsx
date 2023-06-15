67810f6f02f30da2d4040fa2afbec2fea90ede2f
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2023-06-15 15:35:14 +0200

    Update list page, fix missing TRPC wrapper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,28 +1,50 @@
 import "../styles/global.css";
 import "@saleor/macaw-ui/next/style";
 
-import { AppBridgeProvider } from "@saleor/app-sdk/app-bridge";
+import { AppBridgeProvider, useAppBridge } from "@saleor/app-sdk/app-bridge";
 import { RoutePropagator } from "@saleor/app-sdk/app-bridge/next";
 import { ThemeProvider } from "@saleor/macaw-ui/next";
 import { type AppProps } from "next/app";
 
+import { Provider } from "urql";
+import { type ReactNode } from "react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"; // note: it's imported only in dev mode
 import { ThemeSynchronizer } from "../modules/ui/theme-synchronizer";
 import { NoSSRWrapper } from "../modules/ui/no-ssr-wrapper";
 
 import { appBridgeInstance } from "@/app-bridge-instance";
+import { ErrorModal } from "@/modules/ui/organisms/GlobalErrorModal/modal";
+import { trpcClient } from "@/modules/trpc/trpc-client";
+import { createClient } from "@/lib/create-graphq-client";
+
+const UrqlProvider = ({ children }: { children: ReactNode }) => {
+  const { appBridgeState } = useAppBridge();
+  if (!appBridgeState?.saleorApiUrl) {
+    return <>{children}</>;
+  }
+
+  const client = createClient(appBridgeState?.saleorApiUrl, async () =>
+    appBridgeState?.token ? { token: appBridgeState.token } : null,
+  );
+  return <Provider value={client}>{children}</Provider>;
+};
 
 function NextApp({ Component, pageProps }: AppProps) {
   return (
     <NoSSRWrapper>
       <AppBridgeProvider appBridgeInstance={appBridgeInstance}>
-        <ThemeProvider>
-          <ThemeSynchronizer />
-          <RoutePropagator />
-          <Component {...pageProps} />
-        </ThemeProvider>
+        <UrqlProvider>
+          <ThemeProvider>
+            <ThemeSynchronizer />
+            <RoutePropagator />
+            <Component {...pageProps} />
+            <ErrorModal />
+            <ReactQueryDevtools position="top-right" />
+          </ThemeProvider>
+        </UrqlProvider>
       </AppBridgeProvider>
     </NoSSRWrapper>
   );
 }
 
-export default NextApp;
+export default trpcClient.withTRPC(NextApp);
--- src/pages/configurations/list.tsx
+++ src/pages/configurations/list.tsx
@@ -1,8 +1,77 @@
-import { withAuthorization } from "@saleor/app-sdk/app-bridge";
-import { AppLayout } from "@/modules/ui/templates/AppLayout";
+import { useAppBridge, withAuthorization } from "@saleor/app-sdk/app-bridge";
+import { Box, Text } from "@saleor/macaw-ui/next";
+import { AppLayout, AppLayoutRow } from "@/modules/ui/templates/AppLayout";
+import { trpcClient } from "@/modules/trpc/trpc-client";
+import { getErrorHandler } from "@/modules/trpc/utils";
+import { useFetchChannelsQuery } from "generated/graphql";
 
 function ListConfigurationPage() {
-  return <AppLayout title="Stripe">TODO</AppLayout>;
+  const { appBridge } = useAppBridge();
+  const [allConfigurations, channelMappings] = trpcClient.useQueries((t) => [
+    t.paymentAppConfigurationRouter.paymentConfig.getAll(undefined, {
+      onError: getErrorHandler({
+        appBridge,
+        actionId: "list-all-configurations",
+        message: "Error while fetching the list of configurations",
+        title: "API Error",
+      }),
+    }),
+    t.paymentAppConfigurationRouter.mapping.getAll(undefined, {
+      onError: getErrorHandler({
+        appBridge,
+        actionId: "channel-mappings-get-all",
+        message: "Error while fetching the channel mappings",
+        title: "API Error",
+      }),
+    }),
+  ]);
+
+  const [channels] = useFetchChannelsQuery();
+
+  const hasAnyConfigs = allConfigurations.data && allConfigurations.data.length > 0;
+  const hasAnyMappings = Object.values(channelMappings.data || {}).filter(Boolean).length > 0;
+
+  if (allConfigurations.isLoading) {
+    return (
+      <AppLayout title="Stripe">
+        <div />
+      </AppLayout>
+    );
+  }
+
+  return (
+    <AppLayout title="Stripe">
+      <AppLayoutRow
+        title="Stripe Configurations"
+        description="Create Stripe configurations that can be later assigned to Saleor channels."
+      >
+        TODO
+      </AppLayoutRow>
+      <AppLayoutRow
+        disabled={!hasAnyConfigs}
+        title="Saleor channel mappings"
+        description={
+          <Box>
+            <Text as="p" variant="body" size="medium">
+              Assign Stripe configurations to Saleor channels.
+            </Text>
+            {!hasAnyMappings && (
+              <Box marginTop={6}>
+                <Text as="p" variant="body" size="medium" color="textCriticalDefault">
+                  No channels have configurations assigned.
+                </Text>
+                <Text as="p" variant="body" size="medium" color="textCriticalDefault">
+                  This means payments are not processed by Stripe.
+                </Text>
+              </Box>
+            )}
+          </Box>
+        }
+      >
+        TODO
+      </AppLayoutRow>
+    </AppLayout>
+  );
 }
 
 export default withAuthorization()(ListConfigurationPage);
