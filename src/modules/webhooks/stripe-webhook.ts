6a68d4cd5e31ba0ff9784a74fd2956c587e58f9d
Hans Krogh Thomsen <hanskroght@gmail.com>
2024-02-18 16:29:31 +0100

    fix: payment_intent.succeeded webhook handler

--- src/modules/webhooks/stripe-webhook.ts
+++ src/modules/webhooks/stripe-webhook.ts
@@ -437,9 +437,7 @@ function stripePaymentIntentEventToPartialTransactionEventReportMutationVariable
         amount: manualCapture ? paymentIntent.amount_capturable : paymentIntent.amount_received,
         currency: paymentIntent.currency,
       });
-      const type = manualCapture
-        ? TransactionEventTypeEnum.AuthorizationSuccess
-        : TransactionEventTypeEnum.ChargeSuccess;
+      const type = TransactionEventTypeEnum.ChargeSuccess;
 
       return { amount, type, externalUrl, pspReference, message };
     }
